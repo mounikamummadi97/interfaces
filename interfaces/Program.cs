3881428a240dd8462ced2914c1da2f9b9c8d9dbf
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:44:09 +0530

    first commit

---
+++ interfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+interface Icustomer1
+{
+    void print1();
+}
+interface Icustomer2 : Icustomer1
+{
+    void print2();
+}
+public class customer : Icustomer2
+{
+    public void print2()
+    {
+        Console.WriteLine("print2");
+    }
+
+    public void print1()
+    {
+        Console.WriteLine("print1");
+    }
+}
+public class program
+{
+    public static void Main()
+    {
+        customer C1 = new customer();
+        C1.print1();
+    }
+}
